2a797b91f8dc3471e344ccbd03546821476eaa07
pr1203 <123905902+pr1203@users.noreply.github.com>
2023-01-29 19:26:21 -0500

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,16 +1,26 @@
-
 class Book:
-
-        def __init__(self, title, author):
-        self.title = title
+    author = ""
+    title = ""
+   
+    def __init__(self, author, title):
+       
+       
         self.author = author
+        self.title = title
+   
+    def display(self):
+   
+   
+        print (f"{self.title}, written by {self.author}")
+        
 
-        def display(self):
-            print(f"\'{self.title}\', written by {self.author}")
-
+if __name__ == "__main__":
+   
+   
     book1 = Book("J.K. Rowling", "Harry Potter and the Goblet of Fire")
-    book2= Book(" Walter Scott", "Ivanhoe: A Romance")
-
-    book1.display()
-    book2.display()
+    book2 = Book("Sir Walter Scott", "Ivanhoe: A Romance")
+   
+book1.display()
+   
+book2.display()
 
